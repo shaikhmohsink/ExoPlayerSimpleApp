0a9c7a7f244eeecf6858e328260782d9e6709c2a
Mohsin Shaikh <mohsin@dotstudioz.com>
2016-12-12 22:10:32 +0530

    Looping the player to play the same video again and again

--- app/src/main/java/com/mohsin/exoplayersimpleapp/MainActivity.java
+++ app/src/main/java/com/mohsin/exoplayersimpleapp/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
+import android.view.View;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
@@ -39,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Handler handler = new Handler();
-        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter(handler, null);
+        final DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter(handler, null);
+        System.out.println("bandwidthMeter.getBitrateEstimate()bandwidthMeter.getBitrateEstimate():"+bandwidthMeter.getBitrateEstimate());
         /*MediaSource sampleSource = new ExtractorMediaSource(
                 Uri.parse("https://k7q5a5e5.ssl.hwcdn.net/files/company/575729ad97f8152c41a96700/assets/videos/57bca0bf97f815217cd5784e/vod/57bca0bf97f815217cd5784e.m3u8"),
                 new DefaultDataSourceFactory(this, "Android-ExoPlayer", bandwidthMeter),
@@ -62,5 +67,42 @@ public class MainActivity extends AppCompatActivity {
         player.prepare(sampleSource);
         player.setVideoTextureView((TextureView)findViewById(R.id.textureView));
         player.setPlayWhenReady(true);
+
+        player.addListener(new ExoPlayer.EventListener() {
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if(playbackState == ExoPlayer.STATE_BUFFERING) {
+                    System.out.println("buffering bandwidthMeter.getBitrateEstimate():"+bandwidthMeter.getBitrateEstimate());
+                }
+                if(playbackState == ExoPlayer.STATE_READY) {
+                    System.out.println("ready bandwidthMeter.getBitrateEstimate():"+bandwidthMeter.getBitrateEstimate());
+                }
+                if(playbackState == ExoPlayer.STATE_ENDED) {
+                    System.out.println("ExoPlayer.STATE_ENDEDExoPlayer.STATE_ENDEDExoPlayer.STATE_ENDED");
+                    player.seekTo(0);
+                    player.setPlayWhenReady(true);
+                }
+            }
+
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+
+            }
+
+            @Override
+            public void onPositionDiscontinuity() {
+
+            }
+        });
     }
 }
